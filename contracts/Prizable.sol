f8b39e6f6d46aef2d01881f9e65e2d59586ba9a7
Peyman Jahani <jahani.peyman@gmail.com>
2021-09-11 18:36:44 +0430

    Add prizable contract

---
+++ contracts/Prizable.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+
+contract Prizable {
+    // Prizes
+    mapping (address => uint) public prizes;
+
+    // Fallback funciton
+    // No direct payment allowed
+    fallback() external payable {
+        revert();
+    }
+    receive() external payable {
+        revert();
+    }
+
+    function addPrize(address _address, uint _amount) public {
+        uint balance = prizes[_address];
+        balance += _amount;
+        prizes[_address] = balance;
+    }
+
+    function getPrize() public view returns (uint) {
+        return prizes[msg.sender];
+    }
+    
+
+    function withdrawPrize(uint withdrawAmount) public returns (uint) {
+      // If the sender's balance is at least the amount they want to withdraw,
+      // Subtract the amount from the sender's balance, and try to send that amount of ether
+      // to the user attempting to withdraw. 
+      // return the user's balance.
+
+      // 1. Use a require expression to guard/ensure sender has enough funds
+      require(getPrize() >= withdrawAmount);
+  
+      // 2. Transfer Eth to the sender and decrement the withdrawal amount from
+      //    sender's balance
+      prizes[msg.sender] -= withdrawAmount;
+      payable(msg.sender).transfer(withdrawAmount); // TODO
+
+      // 3. Emit the appropriate event for this message
+    //   emit LogWithdrawal(msg.sender, withdrawAmount, getBalance());
+
+      return getPrize();
+    }
+    
+}
