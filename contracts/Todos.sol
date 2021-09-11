2800b56cf5c5c8f93338d15a07f21d932d448065
Peyman Jahani <jahani.peyman@gmail.com>
2021-09-11 15:54:47 +0430

    Update compiler version to 0.8.0

--- contracts/Todos.sol
+++ contracts/Todos.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
-pragma experimental ABIEncoderV2; // Two level dynamic arrays support
+pragma solidity >=0.8.0 <0.9.0;
+//pragma experimental ABIEncoderV2; // Two level dynamic arrays support
 
 contract Todos {
   /*
