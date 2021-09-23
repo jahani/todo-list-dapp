e9e3e6d525f47f3492b29c6ef485d91c45f52acc
Peyman Jahani <jahani.peyman@gmail.com>
2021-09-23 15:00:19 +0330

    Refactor and add a test

--- blockchain/test/todos.js
+++ blockchain/test/todos.js
@@ -3,10 +3,33 @@ const Todos = artifacts.require("Todos");
 
 contract("Todos", function ( accounts ) {
 
-  it("should be owner", async function () {
-    const todos = await Todos.deployed();
-    const value = await todos.owner();
-    return assert.equal(value, accounts[0]);
+  const zeroDueDate = 0;
+  const wrongDueDate = 1;
+  const rightDueDate = 4294967295; // 2^32 - 1
+
+  let instance;
+
+  beforeEach(async () => {
+    instance = await Todos.new();
+  });
+
+  it("should define owner", async function () {
+    const value1 = await instance.owner();
+    const value2 = accounts[0];
+    return assert.equal(value1, value2);
+  });
+  
+  it("should accept zero due date", async function () {
+    await instance.add('Task #0', zeroDueDate);
+  });
+
+
+  it("should allow completeing non-prized task with right due date", async function () {
+    await instance.add('Task #0', rightDueDate);
+    await instance.setComplete(0);
+    const tasks = await instance.getTasks();
+    const task = tasks[0];
+    assert.isTrue(task.completed);
   });
 
 });
