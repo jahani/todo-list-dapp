db0f77fe262c4096f4de4ffa297ed371e869c25c
Peyman Jahani <jahani.peyman@gmail.com>
2021-10-18 17:15:28 +0330

    Add GitHub link

--- client/src/App.vue
+++ client/src/App.vue
@@ -68,12 +68,9 @@
       <footer class="py-3 my-4">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
           <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+          <li class="nav-item"><a href="https://github.com/jahani/todo-list-dapp" class="nav-link px-2 text-muted">GitHub</a></li>
         </ul>
-        <p class="text-center text-muted">© 2021 Company, Inc</p>
+        <p class="text-center text-muted">© 2021 No Company, Inc</p>
       </footer>
     </div>
 
